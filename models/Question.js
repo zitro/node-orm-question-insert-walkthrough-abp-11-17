e3a8202863866a6db73f5e13a9174df35cef481b
aviflombaum <avi@flombaum.com>
2017-11-17 01:25:11 -0500

    Solved

--- models/Question.js
+++ models/Question.js
@@ -18,6 +18,16 @@ class Question{
     this.content = content
   }
 
+  insert(){
+    const self = this // THIS IS THE CRUX
+    const sql = `INSERT INTO questions (content) VALUES (?)`
+    return new Promise(function(resolve){
+      db.run(sql, [self.content], function(err, result){
+        self.id = this.lastID
+        resolve(self)      
+      })
+    })
+  }
 }
 
 module.exports = Question;
